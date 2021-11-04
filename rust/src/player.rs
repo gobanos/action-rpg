f0b41d8a850a4611c47bf185c234ae9b5a231395
Grégory OBANOS <gregory.obanos@gmail.com>
2021-11-04 19:16:59 +0100

    Part 6

--- rust/src/player.rs
+++ rust/src/player.rs
@@ -1,7 +1,6 @@
 use gdnative::prelude::*;
 use gdnative_api::utils::NodeExt;
 use gdnative_api::*;
-use strum::AsRefStr;
 
 const ACCELERATION: f32 = 500.0;
 const FRICTION: f32 = 500.0;
@@ -13,8 +12,8 @@ const MAX_SPEED: f32 = 80.0;
 #[register_with(Self::register_builder)]
 pub struct Player {
     velocity: Vector2,
-    animation_player: Option<Ref<AnimationPlayer>>,
-    animation: PlayerAnimation,
+    animation_tree: Option<Ref<AnimationTree>>,
+    animation_state: Option<Ref<AnimationNodeStateMachinePlayback>>,
 }
 
 // __One__ `impl` block can have the `#[methods]` attribute, which will generate
@@ -31,28 +30,39 @@ impl Player {
         godot_print!("Player is created!");
         Player {
             velocity: Vector2::zero(),
-            animation_player: None,
-            animation: PlayerAnimation::default(),
+            animation_tree: None,
+            animation_state: None,
         }
     }
 
-    /// Access the AnimationPlayer node.
+    /// Access the AnimationTree node.
     /// Panics if called before `_ready`.
-    fn animation_player(&self) -> TRef<AnimationPlayer> {
-        unsafe { self.animation_player.unwrap().assume_safe() }
+    fn animation_tree(&self) -> TRef<AnimationTree> {
+        unsafe { self.animation_tree.as_ref().unwrap().assume_safe() }
+    }
+
+    /// Access the AnimationNodeStateMachinePlayback node.
+    /// Panics if called before `_ready`.
+    fn animation_state(&self) -> TRef<AnimationNodeStateMachinePlayback> {
+        unsafe { self.animation_state.as_ref().unwrap().assume_safe() }
     }
 
     #[export]
     fn _ready(&mut self, owner: &KinematicBody2D) {
-        self.animation_player = Some(unsafe {
+        self.animation_tree = Some(unsafe {
             owner
-                .get_node_as::<AnimationPlayer>("AnimationPlayer")
-                .expect("AnimationPlayer node not found")
+                .get_node_as::<AnimationTree>("AnimationTree")
+                .expect("AnimationTree node not found")
                 .claim()
         });
-
-        self.animation_player()
-            .play(self.animation.name(), -1.0, 1.0, false);
+        self.animation_state = Some(
+            self.animation_tree()
+                .get("parameters/playback")
+                .try_to_object::<AnimationNodeStateMachinePlayback>()
+                .expect(
+                    "AnimationTree parameters/playback is not a AnimationNodeStateMachinePlayback",
+                ),
+        );
     }
 
     // This function will be called in every frame
@@ -65,16 +75,18 @@ impl Player {
         );
 
         self.velocity = if input_vector != Vector2::zero() {
+            self.animation_tree()
+                .set("parameters/Idle/blend_position", input_vector);
+            self.animation_tree()
+                .set("parameters/Run/blend_position", input_vector);
+            self.animation_state().travel("Run");
             self.velocity
                 .move_towards(input_vector.normalize() * MAX_SPEED, ACCELERATION * delta)
         } else {
+            self.animation_state().travel("Idle");
             self.velocity
                 .move_towards(Vector2::zero(), FRICTION * delta)
         };
-
-        self.animation.update_from_velocity(self.velocity);
-        self.animation_player()
-            .play(self.animation.name(), -1.0, 1.0, false);
 
         self.velocity = owner.move_and_slide(
             self.velocity,
@@ -85,69 +97,4 @@ impl Player {
             true,
         );
     }
-}
-
-#[derive(Debug, Copy, Clone)]
-struct PlayerAnimation {
-    status: Status,
-    direction: Direction,
-}
-
-impl PlayerAnimation {
-    fn name(self) -> String {
-        format!("{}{}", self.status.as_ref(), self.direction.as_ref())
-    }
-
-    fn update_from_velocity(&mut self, velocity: Vector2) {
-        *self = if velocity == Vector2::zero() {
-            PlayerAnimation {
-                status: Status::Idle,
-                direction: self.direction,
-            }
-        } else {
-            let Vector2 { x, y, .. } = velocity;
-            if x.abs() >= y.abs() {
-                PlayerAnimation {
-                    status: Status::Run,
-                    direction: if x > 0.0 {
-                        Direction::Right
-                    } else {
-                        Direction::Left
-                    },
-                }
-            } else {
-                PlayerAnimation {
-                    status: Status::Run,
-                    direction: if y > 0.0 {
-                        Direction::Down
-                    } else {
-                        Direction::Up
-                    },
-                }
-            }
-        };
-    }
-}
-
-impl Default for PlayerAnimation {
-    fn default() -> Self {
-        PlayerAnimation {
-            status: Status::Idle,
-            direction: Direction::Right,
-        }
-    }
-}
-
-#[derive(AsRefStr, Debug, Copy, Clone)]
-enum Status {
-    Idle,
-    Run,
-}
-
-#[derive(AsRefStr, Debug, Copy, Clone)]
-enum Direction {
-    Left,
-    Right,
-    Up,
-    Down,
 }
